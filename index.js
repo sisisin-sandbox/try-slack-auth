7be4297a29a7c9fd4ed93bda7fa5f1810778b73e
sisisin <azsisisin@gmail.com>
2018-10-29 10:34:00 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const { CLIENT_ID, CLIENT_SECRET, PORT } = process.env,
+  SlackStrategy = require('@aoberoi/passport-slack').default.Strategy,
+  passport = require('passport'),
+  express = require('express'),
+  app = express();
+
+passport.use(
+  new SlackStrategy(
+    {
+      clientID: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+    },
+    (accessToken, scopes, team, bot, user, done) => {
+      console.log(accessToken, scopes, team, bot, user, done);
+      return done(null, user);
+    },
+  ),
+);
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+app.use(passport.initialize());
+app.use(express.static('public'));
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.render('index.html'));
+app.get('/auth/slack', passport.authenticate('slack'));
+app.get(
+  '/auth/slack/callback',
+  passport.authenticate('slack', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  },
+);
+app.listen(PORT || 3000);
